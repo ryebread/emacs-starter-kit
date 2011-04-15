308a8a8d2c4381e3ce6a9dc3457ab131600fe4dc
ryebread <hhdslb@gmail.com>
2011-04-15 17:04:38 +0800

    add color-theme-ir-bblack pack

--- mine-kit-el-get.el
+++ mine-kit-el-get.el
@@ -78,6 +78,7 @@
    ;;        color-theme-zenburn
    color-theme-twilight
    color-theme-subdued
+   color-theme-ir-black
    ;;        color-theme-railscasts
 
    ;;         ;; XML and HTML
@@ -145,7 +146,7 @@
    ;;         google-weather
 
    ;; Python: you Should manual install python package-rope,pymacs,pep8,ropemode
-   ;;         (:name python-mode :type http :url "http://launchpad.net/python-mode/trunk/5.1.0/+download/python-mode.el") ; Overridden because bzr's lp source breaks with python 2.7 (https://bugs.launchpad.net/bzr/+bug/612096)
+
    ;;   python-mode   ;conflict with yasnippet
    pylookup
    pymacs
